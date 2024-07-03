e8575f14f6b97f6a676a05650110e768fe4f01f0
Jourdelune <64205064+Jourdelune@users.noreply.github.com>
2024-07-03 10:36:39 +0200

    Update dac.py

--- dac/model/dac.py
+++ dac/model/dac.py
@@ -241,10 +241,10 @@ class DAC(BaseModel, CodecMixin):
                 Number of samples in input audio
         """
         z = self.encoder(audio_data)
-        z, codes, latents, commitment_loss, codebook_loss = self.quantizer(
+        z_q, codes, latents, commitment_loss, codebook_loss = self.quantizer(
             z, n_quantizers
         )
-        return z, codes, latents, commitment_loss, codebook_loss
+        return z, z_q, codes, latents, commitment_loss, codebook_loss
 
     def decode(self, z: torch.Tensor):
         """Decode given latent codes and return audio data
